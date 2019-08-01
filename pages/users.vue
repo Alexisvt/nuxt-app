f952cc870f6e82363b285df962146e6fa42ecb80
Alexis Villegas Torres <alexisvt@gmail.com>
2019-07-31 22:21:51 -0600

    add sample pages and layouts

---
+++ pages/users.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <input v-model="userId" type="text" />
+    <button @click="onLoadUser">Load user</button>
+    <nuxt-child></nuxt-child>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      userId: ''
+    }
+  },
+  methods: {
+    onLoadUser() {
+      this.$router.push(`/users/${this.userId}`)
+    }
+  },
+  layout: 'users'
+}
+</script>
